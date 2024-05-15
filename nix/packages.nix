90543d7fa6e0743da2487a8535cc014d08c9fcae
Gregor Grigorjan <gregor@grigorjan.net>
2024-05-15 05:13:38 +0300

    feat(test): add el-patch template test

--- nix/packages.nix
+++ nix/packages.nix
@@ -5,7 +5,7 @@ _: {
       packages = rec {
         default = magit-file-icons;
 
-        magit-file-icons = pkgs.emacsPackages.trivialBuild {
+        magit-file-icons = pkgs.emacsPackages.trivialBuild rec {
           pname = "magit-file-icons";
           version = "v0.0.1";
 
@@ -16,6 +16,22 @@ _: {
             pkgs.emacsPackages.magit
             pkgs.emacsPackages.nerd-icons
           ];
+
+          doCheck = true;
+
+          nativeCheckInputs = [
+            pkgs.git
+            pkgs.emacs
+          ] ++ packageRequires;
+
+          checkPhase = ''
+            output=$(emacs --batch -L . -L test -l magit-file-icons-tests 2> >(grep invalid))
+            if [[ $output ]]; then
+                echo "Failed to validate templates. Output:"
+                echo "$output"
+                exit 1
+            fi
+          '';
         };
       };
     };
