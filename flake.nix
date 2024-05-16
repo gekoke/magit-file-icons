8d5e508a71a16f83137e5385f9c3780c9e72a74b
Gregor Grigorjan <gregor@grigorjan.net>
2024-05-17 02:26:46 +0300

    feat: use `emacs-overlay`

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
     systems.url = "github:nix-systems/default";
+
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -15,7 +18,19 @@
       imports = [
         ./nix/dev-shells.nix
         ./nix/packages.nix
+        (
+          { inputs, ... }:
+          {
+            perSystem =
+              { pkgs, system, ... }:
+              {
+                _module.args.pkgs = import inputs.nixpkgs {
+                  inherit system;
+                  overlays = [ inputs.emacs-overlay.overlays.default ];
+                };
+              };
+          }
+        )
       ];
     };
 }
-
