cfb8acd24b047bf9fdc8b17915f5170cdc24e245
Gregor Grigorjan <gregor@grigorjan.net>
2024-05-17 02:57:19 +0300

    feat(test): add basic `magit` tests
    So we can be sure the status buffer opens without errors

--- test/magit-file-icons-tests.el
+++ test/magit-file-icons-tests.el
@@ -4,8 +4,27 @@
 (require 'el-patch)
 (require 'magit-file-icons)
 
-(ert-deftest magit-file-icons-should-have-no-invalid-templates ()
+(ert-deftest magit-file-icons-test-has-no-invalid-templates ()
   (el-patch-validate-all-templates))
+
+(ert-deftest magit-file-icons-test-can-open-magit-status-buffer-while-in-minor-mode ()
+  (magit-file-icons-mode +1)
+  (magit-status-setup-buffer))
+
+(ert-deftest magit-file-icons-test-can-enable-minor-mode-in-magit-status-buffer ()
+  (magit-status-setup-buffer)
+  (magit-file-icons-mode +1))
+
+(ert-deftest magit-file-icons-test-can-disable-minor-mode-in-magit-status-buffer ()
+  (magit-file-icons-mode +1)
+  (magit-status-setup-buffer)
+  (magit-file-icons-mode -1))
+
+(ert-deftest magit-file-icons-test-can-toggle-minor-mode-in-magit-status-buffer ()
+  (magit-file-icons-mode +1)
+  (magit-status-setup-buffer)
+  (magit-file-icons-mode 'toggle)
+  (magit-file-icons-mode 'toggle))
 
 ;; Local Variables:
 ;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
