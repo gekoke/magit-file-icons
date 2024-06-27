861670d448df4fba6fb993941d41953cee230a7b
Gregor Grigorjan <gregor@grigorjan.net>
2024-06-27 15:28:28 +0300

    chore: bump version

--- nix/packages.nix
+++ nix/packages.nix
@@ -7,7 +7,7 @@ _: {
 
         magit-file-icons = pkgs.emacsPackages.trivialBuild rec {
           pname = "magit-file-icons";
-          version = "v1.0.2";
+          version = "v2.0.0";
 
           src = ../.;
 
